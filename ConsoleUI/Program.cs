086d51093cb2c786cb6c25af97867f162b33c578
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2021-01-31 16:41:49 +0200

    Commit All

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Marka : " + car.BrandId);
+                Console.WriteLine("Açıklama : " + car.Description);
+                Console.WriteLine("Fiyatı : " + car.DailyPrice);
+                Console.WriteLine("Model yılı : " + car.ModelYear);
+                Console.WriteLine("***************************************************************");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id=1, BrandId=1, ColorId=1, DailyPrice=55000, ModelYear= 2005, Description="Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir."},
+                new Car{ Id=2, BrandId=1, ColorId=1, DailyPrice=65000, ModelYear= 2020, Description="Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. "},
+                new Car{ Id=3, BrandId=2, ColorId=2, DailyPrice=80000, ModelYear= 2023, Description="Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır."},
+                new Car{ Id=4, BrandId=2, ColorId=2, DailyPrice=30000, ModelYear= 2001, Description="1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur."},
+                new Car{ Id=5, BrandId=3, ColorId=3, DailyPrice=500000, ModelYear= 2050, Description="Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz."},
+                new Car{ Id=65, BrandId=3, ColorId=3, DailyPrice=450000, ModelYear= 2015, Description="Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır."}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
